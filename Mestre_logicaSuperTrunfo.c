858fcd2a2bbd1126149b82c25d90d1ef9e92950f
Lucas Marques <lukasmarqgomes10@outlook.com>
2025-10-23 23:42:00 -0300

    atualizacao

--- Mestre_logicaSuperTrunfo.c
+++ Mestre_logicaSuperTrunfo.c
@@ -2,7 +2,7 @@
 // teste
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
+// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
